4091fdfa8348db654cb4615c8e885ddb354258f1
lzxue <120635640@qq.com>
2023-11-10 22:20:31 +0800

    chore: 移除 lib

--- lib/map.d.ts
+++
@@ -1,6 +0,0 @@
-import { Map } from 'leaflet';
-import { BaseMapWrapper } from '@antv/l7';
-import MapService from './lib/map';
-export default class MapboxWrapper extends BaseMapWrapper<Map> {
-  protected getServiceConstructor(): typeof MapService;
-}
